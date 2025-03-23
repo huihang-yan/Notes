9c2fe454a531bca05ceac030d621cd4a3c85b148
Huihang Yan <huihang.yan@outlook.com>
2025-03-23 18:57:26 +0800

    Sun Mar 23 18:57:26 CST 2025 @ Linux darkstar.example.net 5.15.19-smp #1 SMP PREEMPT Wed Feb 2 01:25:47 CST 2022 i686 Intel(R) Celeron(R) CPU 1037U @ 1.80GHz GenuineIntel GNU/Linux

--- conf-wifi.c
+++ conf-wifi.c
@@ -7,7 +7,10 @@ int main(void)
 	char *name, *psk;
 	while (2 == scanf("%ms %ms", &name, &psk))
 	{
-		printf("%s %s\n", name, psk);
+		printf("network={\n"
+		       "	ssid=\"%s\"\n"
+		       "	psk=\"%s\"\n"
+		       "}\n\n", name, psk);
 		free(name); free(psk);
 	}
 	return 0;
